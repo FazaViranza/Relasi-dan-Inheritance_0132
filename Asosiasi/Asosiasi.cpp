d122b72c88f9b0706470e4b8d1eec46ede4ef3db
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-05-27 18:53:27 +0700

    pembuatan kelas dan prosedur asosiasi

---
+++ Asosiasi/Asosiasi.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+class pasien{   //Asosiasi
+    public:
+        string nama;
+        vector<dokter*> daftar_dokter;
+        pasien(string pNama): nama (pNama) {
+            cout << "Pasien \"" << nama << "\" ada\n";
+        }
+        ~pasien(){
+            cout << "Pasien \"" << nama
+            << "\" tidak ada\n";
+        }
+
+        void tambahDokter(dokter*);
+        void cetakDokter();
+};
+
+class dokter {
+    public:
+        string nama;
+        vector<pasien*> daftar_pasien;
+
+        dokter(string pNama): nama (pNama) {
+            cout << "Dokter \"" << nama << "\" ada\n";
+        }
+         ~dokter(){
+            cout << "Dokter \"" << nama
+            << "\" tidak ada\n";
+        }
+
+        void tambahPasien(pasien*);
+        void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+    cout<< "Daftar dokter yang menangani pasien \""
+    << this->nama <<"\":\n";
+    //auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
