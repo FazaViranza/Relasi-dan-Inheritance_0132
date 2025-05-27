a4574d61832b567a0cd7542c8ad9efee26681ec3
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-05-27 20:06:33 +0700

    Final Inheritance

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -3,36 +3,39 @@
 using namespace std;
 
 class orang {
-    public:
-        string nama;
+public:
+    string nama;
 
-        orang(string pNama):
-            nama(pNama){
-                cout<< "orang dibuat\n" << endl;
-            }
-        ~orang(){
-            cout<< "orang dihapus\n" << endl;
-        }
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
 
     int jumlah(int a, int b) {
         return a + b;
     }
-
+    
 };
 
 class pelajar : public orang {
-    public:
-        string sekolah;
+public:
+    string sekolah;
 
-        pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
-            cout << "pelajar dibuat\n" << endl;
-        }
-        ~pelajar() {
-            cout << "pelajar dihapus\n" << endl;
-        }
-        string perkenalan(){
-              return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n
-\n"}
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
 };
 
 int main() {
